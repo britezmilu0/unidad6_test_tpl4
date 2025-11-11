822435ebe3c845a9672b33d97e02120ac93253a6
Milagros Britez <britezmilu0@gmail.com>
2025-11-11 12:42:51 -0300

    se realizo un test unitario

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+
+import express from 'express';
+import { TodoService } from './services/todo.service';
+import { buildTodoRouter } from './routes/todo.routes';
+
+const app = express();
+app.use(express.json());
+
+const todoService = new TodoService();
+app.use('/todos', buildTodoRouter(todoService));
+
+app.get('/health', (_req, res) => res.json({ ok: true }));
+
+// Exponer el servicio para testing (resetear estado)
+(app as any).todoService = todoService;
+
+export default app;
